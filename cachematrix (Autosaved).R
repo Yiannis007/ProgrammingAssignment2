569047a01ad085fd6fb9ef383064f5290403a4a7
Yiannis007 <yiannischardaloupas@gmail.com>
2014-11-18 18:15:47 +0200

    Courser R assignment 2

---
+++ cachematrix (Autosaved).R
@@ -0,0 +1,23 @@
+## These functions cache the matrix inversion. 
+
+
+
+
+## The makeCacheMatrix function performs the following functions:
+## 1) set the matrix
+## 2) get the matrix
+## 3) set the inverted matrix
+## 4) get the inverted matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+	inv<-matrix()
+	
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
